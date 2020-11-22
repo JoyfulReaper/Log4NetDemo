efbe5df8005b3fda70bfadf4eb5e9c14674e4c69
Kyle Givler <xvegan88x@gmail.com>
2020-11-22 18:50:54 -0500

    Add file Appenders

--- Log4NetTutorial/ConsoleUI/Program.cs
+++ Log4NetTutorial/ConsoleUI/Program.cs
@@ -11,7 +11,8 @@ namespace ConsoleUI
     class Program
     {
         //private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Program.cs"); // Logger for each class, give name based on class
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType); // Or use reflection
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType); // Or use reflection (before .NET framework 4.6)
+        private static readonly log4net.ILog log = LogHelper.GetLogger(); // .NET 4.6 framework and beyond - full file path
 
         static void Main(string[] args)
         {
